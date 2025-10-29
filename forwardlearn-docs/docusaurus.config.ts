21f30739cf3fcf9d79e9f7c1f95a8be7a0ec0b3b
Shawn Blanchard | Tech Comms Specialist <docu_pro_solutions@icloud.com>
2025-10-29 15:48:49 -0700

    Update Docusaurus configuration file

--- forwardlearn-docs/docusaurus.config.ts
+++ forwardlearn-docs/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'ForwardLearn',
+  tagline: 'Dinosaurs are coDevSecOps, NIST RMF, and cATO',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -22,8 +22,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'docupro23', // Usually your GitHub org/user name.
+  projectName: 'forwardlearn-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
